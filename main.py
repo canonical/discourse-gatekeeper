f8c72a3286ee082c330a60c69a48221fe8c26d14
jdkandersson <david.andersson@canonical.com>
2022-11-15 14:45:07 +1100

    include cearting index page

--- main.py
+++ main.py
@@ -4,12 +4,30 @@
 """Main execution for the action."""
 
 import os
+import pathlib
+
+from src.discourse import create_discourse
+from src.server import retrieve_or_create_index
 
 
 def main():
     """Execute the action."""
-    create_new_input = os.environ["INPUT_CREATE_NEW_TOPIC"]
-    print(create_new_input)
+    # Read input
+    create_new_topic = os.environ["INPUT_CREATE_NEW_TOPIC"] == "true"
+    discourse_host = os.environ["INPUT_DISCOURSE_HOST"]
+    discourse_category_id = int(os.environ["INPUT_DISCOURSE_CATEGORY_ID"])
+    print(create_new_topic)
+    print(discourse_host)
+    print(discourse_category_id)
+
+    # Execute action
+    discourse = create_discourse(hostname=discourse_host, category_id=discourse_category_id)
+    page = retrieve_or_create_index(
+        create_if_not_exists=create_new_topic,
+        local_base_path=pathlib.Path(),
+        server_client=discourse,
+    )
+    print(page)
 
 
 if __name__ == "__main__":
