9463d3f098425d23fe91d211f322f7df1d205830
jdkandersson <david.andersson@canonical.com>
2022-11-17 16:28:48 +1100

    add module for parsing navigation table

--- src/navigation_table.py
+++ src/navigation_table.py
@@ -3,8 +3,44 @@
 
 """Class for parsing and rendering a navigation table."""
 
-from pathlib import Path
 import typing
+import re
+
+from .exceptions import NavigationTableParseError
+
+
+_WHITESPACE = r"\s*"
+_TABLE_HEADER_REGEX = (
+    r"\|"
+    rf"{_WHITESPACE}level{_WHITESPACE}\|"
+    rf"{_WHITESPACE}path{_WHITESPACE}\|"
+    rf"{_WHITESPACE}navlink{_WHITESPACE}\|"
+)
+_TABLE_HEADER_PATTERN = re.compile(_TABLE_HEADER_REGEX, re.IGNORECASE)
+_TABLE_PATTERN = re.compile(rf"[\s\S]*{_TABLE_HEADER_REGEX}[\s\S]*\|?", re.IGNORECASE)
+_FILLER_ROW_REGEX_COLUMN = rf"{_WHITESPACE}-+{_WHITESPACE}\|"
+_FILLER_ROW_PATTERN = re.compile(rf"\|{_FILLER_ROW_REGEX_COLUMN * 3}")
+_LEVEL_REGEX = rf"{_WHITESPACE}(\d+){_WHITESPACE}"
+_PATH_REGEX = rf"{_WHITESPACE}([\w-]+){_WHITESPACE}"
+_NAVLINK_TITLE_REGEX = r"[\w\- ]+?"
+_NAVLINK_LINK_REGEX = r"[\w\/-]*"
+_NAVLINK_REGEX = (
+    rf"{_WHITESPACE}\[{_WHITESPACE}({_NAVLINK_TITLE_REGEX}){_WHITESPACE}\]{_WHITESPACE}"
+    rf"\({_WHITESPACE}({_NAVLINK_LINK_REGEX}){_WHITESPACE}\){_WHITESPACE}"
+)
+_ROW_PATTERN = re.compile(rf"{_WHITESPACE}\|{_LEVEL_REGEX}\|{_PATH_REGEX}\|{_NAVLINK_REGEX}\|")
+
+
+class Navlink(typing.NamedTuple):
+    """Represents navlink of a table row of the navigation table.
+
+    Attrs:
+        title: The title of the documentation page.
+        link: The relative URL to the documentation page or None if there is no link.
+    """
+
+    title: str
+    link: str | None
 
 
 class TableRow(typing.NamedTuple):
@@ -13,31 +49,79 @@ class TableRow(typing.NamedTuple):
     Attrs:
         level: The number of parents, is 1 if there is no parent.
         path: The a unique string identifying the row.
-        navlink: The relative URL to the documentation page.
-        file_path: The path to the file relative to the docs folder.
+        navlink: The title and relative URL to the documentation page.
     """
 
     level: int
     path: str
-    navlink: str
-    file_path: Path
+    navlink: Navlink
 
 
-class NavigationTable:
-    """Parses and can render a navigation table.
+def _filter_line(line: str) -> bool:
+    """Check whether a line should be parsed.
 
-    Attrs:
-        rows: All the rows of the table.
+    Args:
+        line: The line to check.
+
+    Returns:
+        Whether the line should be parsed.
     """
+    if _TABLE_HEADER_PATTERN.match(line) is not None:
+        return True
+    if _FILLER_ROW_PATTERN.match(line) is not None:
+        return True
+    if _ROW_PATTERN.match(line) is not None:
+        return False
+    return True
 
-    rows: list[TableRow]
-    table_regex = r"(\|.*Level.*\|.*Path.*\|.*Navlink.*\|[\s\S]*\|)"
 
-    def __init__(self, rows: list[TableRow]) -> None:
-        """Construct.
+def _line_to_row(line: str) -> TableRow:
+    """Parse a markdown table line.
 
-        Recommend the use of one of the from_* methods.
+    Raises NavigationTableParseError if no match is found for the line.
 
-        Args:
-            rows: The rows of the table.
-        """
+    Args:
+        line: The line to process.
+
+    Returns:
+        The parsed row.
+    """
+    match = _ROW_PATTERN.match(line)
+
+    if match is None:
+        raise NavigationTableParseError(f"Invalid table row, {line=!r}")
+
+    level = int(match.group(1))
+    path = match.group(2)
+    navlink_title = match.group(3)
+    navlink_link = match.group(4)
+
+    return TableRow(
+        level=level,
+        path=path,
+        navlink=Navlink(title=navlink_title, link=navlink_link if navlink_link else None),
+    )
+
+
+def from_page(page: str) -> list[TableRow]:
+    """Create an instance based on a markdown page.
+
+    Algorithm:
+        1.  Extract the table based on a regular expression looking for a 3 column table with
+            the headers level, path and navlink (case insensitive). If the table is not found,
+            assume that it is equivalent to a table without rows.
+        2.  Process the rows line by line:
+            2.1. If the row matches the header or filler pattern, skip it.
+            2.2. Extract the level, path and navlink values.
+
+    Args:
+        page: The page to extract the rows from.
+
+    """
+    match = _TABLE_PATTERN.match(page)
+
+    if match is None:
+        return []
+
+    table = match.group(0)
+    return [_line_to_row(line) for line in table.splitlines() if not _filter_line(line)]
---
+++ tests/unit/test_navigation_table.py
@@ -0,0 +1,239 @@
+# Copyright 2022 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+"""Unit tests for run module."""
+
+# Need access to protected functions for testing
+# pylint: disable=protected-access
+
+import pytest
+
+from src import navigation_table
+from src.exceptions import NavigationTableParseError
+
+
+@pytest.mark.parametrize(
+    "line, expected_result",
+    [
+        pytest.param("", True, id="empty"),
+        pytest.param("unmatched line", True, id="does not match any line regex"),
+        pytest.param("||||", True, id="line with nothing"),
+        pytest.param("|level|path|navlink|", True, id="matches the header lower case"),
+        pytest.param("|LEVEL|PATH|NAVLINK|", True, id="matches the header upper case"),
+        pytest.param("|Level|Path|Navlink|", True, id="matches the header mixed case"),
+        pytest.param(" |level|path|navlink|", True, id="matches the header single leading space"),
+        pytest.param(
+            "  |level|path|navlink|", True, id="matches the header multiple leading space"
+        ),
+        pytest.param("\t|level|path|navlink|", True, id="matches the header leading tab"),
+        pytest.param("| level|path|navlink|", True, id="matches the header space before level"),
+        pytest.param("|level |path|navlink|", True, id="matches the header space after level"),
+        pytest.param("|level| path|navlink|", True, id="matches the header space before path"),
+        pytest.param("|level|path |navlink|", True, id="matches the header space after path"),
+        pytest.param("|level|path| navlink|", True, id="matches the header space before navlink"),
+        pytest.param("|level|path|navlink |", True, id="matches the header space after navlink"),
+        pytest.param("|level|path|navlink| ", True, id="matches the header trailing space"),
+        pytest.param("|-|-|-|", True, id="matches filler single dash"),
+        pytest.param("|--|--|--|", True, id="matches filler multiple dash"),
+        pytest.param(" |-|-|-|", True, id="matches filler leading space"),
+        pytest.param("| -|-|-|", True, id="matches filler space before first column"),
+        pytest.param("|- |-|-|", True, id="matches filler space after first column"),
+        pytest.param("|-| -|-|", True, id="matches filler space before second column"),
+        pytest.param("|-|- |-|", True, id="matches filler space after second column"),
+        pytest.param("|-|-| -|", True, id="matches filler space before third column"),
+        pytest.param("|-|-|- |", True, id="matches filler space after third column"),
+        pytest.param("|-|-|-| ", True, id="matches filler trailing space"),
+        pytest.param("||a|[a]()|", True, id="first column empty"),
+        pytest.param("|a|a|[a]()|", True, id="first column character"),
+        pytest.param("|1||[a]()|", True, id="second column empty"),
+        pytest.param("|1|/|[a]()|", True, id="second column forward slash"),
+        pytest.param("|1|a||", True, id="third column empty"),
+        pytest.param("|1|a|a]()|", True, id="third column leading square bracket missing"),
+        pytest.param("|1|a|[]()|", True, id="third column title missing"),
+        pytest.param("|1|a|[a()|", True, id="third column closing square bracket missing"),
+        pytest.param("|1|a|[a])|", True, id="third column opening link bracket missing"),
+        pytest.param(r"|1|a|[a](\)|", True, id="third column link includes backslash"),
+        pytest.param("|1|a|[a](|", True, id="third column closing link bracket missing"),
+        pytest.param("|1|a|[a]()|", False, id="matches row"),
+    ],
+)
+def test__filter_line(line, expected_result):
+    """
+    arrange: given line and expected return value
+    act: when _filter_line is called with the line
+    assert: then the expected return value is returned.
+    """
+    returned_result = navigation_table._filter_line(line)
+
+    assert returned_result == expected_result
+
+
+@pytest.mark.parametrize(
+    "line, expected_result",
+    [
+        pytest.param("|1|a|[b]()|", (1, "a", ("b", None)), id="simple"),
+        pytest.param(" |1|a|[b]()|", (1, "a", ("b", None)), id="simple leading space"),
+        pytest.param("| 1|a|[b]()|", (1, "a", ("b", None)), id="simple space before first column"),
+        pytest.param("|1 |a|[b]()|", (1, "a", ("b", None)), id="simple space after first column"),
+        pytest.param(
+            "|1| a|[b]()|", (1, "a", ("b", None)), id="simple space before second column"
+        ),
+        pytest.param("|1|a |[b]()|", (1, "a", ("b", None)), id="simple space after second column"),
+        pytest.param("|1|a| [b]()|", (1, "a", ("b", None)), id="simple space before third column"),
+        pytest.param("|1|a|[b]() |", (1, "a", ("b", None)), id="simple space after third column"),
+        pytest.param("|1|a|[b]()| ", (1, "a", ("b", None)), id="simple trailing space"),
+        pytest.param("|12|a|[b]()|", (12, "a", ("b", None)), id="first column multiple digits"),
+        pytest.param(
+            "|1|az|[b]()|", (1, "az", ("b", None)), id="second column multiple characters"
+        ),
+        pytest.param("|1|A|[b]()|", (1, "A", ("b", None)), id="second column upper case"),
+        pytest.param("|1|2|[b]()|", (1, "2", ("b", None)), id="second column digit"),
+        pytest.param("|1|_|[b]()|", (1, "_", ("b", None)), id="second column underscore"),
+        pytest.param("|1|-|[b]()|", (1, "-", ("b", None)), id="second column dash"),
+        pytest.param(
+            "|1|a|[bz]()|", (1, "a", ("bz", None)), id="third column title multiple characters"
+        ),
+        pytest.param(
+            "|1|a|[B]()|", (1, "a", ("B", None)), id="third column title capital character"
+        ),
+        pytest.param("|1|a|[2]()|", (1, "a", ("2", None)), id="third column title digit"),
+        pytest.param("|1|a|[_]()|", (1, "a", ("_", None)), id="third column title underscore"),
+        pytest.param("|1|a|[-]()|", (1, "a", ("-", None)), id="third column title dash"),
+        pytest.param("|1|a|[ ]()|", (1, "a", (" ", None)), id="third column title space"),
+        pytest.param("|1|a|[ b]()|", (1, "a", ("b", None)), id="third column title space before"),
+        pytest.param("|1|a|[b ]()|", (1, "a", ("b", None)), id="third column title space after"),
+        pytest.param(
+            "|1|a|[b c]()|", (1, "a", ("b c", None)), id="third column title embedded space"
+        ),
+        pytest.param("|1|a|[b](c)|", (1, "a", ("b", "c")), id="third column link defined"),
+        pytest.param(
+            "|1|a|[b] (c)|",
+            (1, "a", ("b", "c")),
+            id="third column white spave between title and link",
+        ),
+        pytest.param(
+            "|1|a|[b]( c)|", (1, "a", ("b", "c")), id="third column link whitespace before"
+        ),
+        pytest.param(
+            "|1|a|[b](c )|", (1, "a", ("b", "c")), id="third column link whitespace after"
+        ),
+        pytest.param(
+            "|1|a|[b](cd)|", (1, "a", ("b", "cd")), id="third column link multiple characters"
+        ),
+        pytest.param("|1|a|[b](C)|", (1, "a", ("b", "C")), id="third column link upper case"),
+        pytest.param("|1|a|[b](2)|", (1, "a", ("b", "2")), id="third column link digit"),
+        pytest.param("|1|a|[b](/)|", (1, "a", ("b", "/")), id="third column link forward slash"),
+        pytest.param("|1|a|[b](-)|", (1, "a", ("b", "-")), id="third column link dash"),
+    ],
+)
+def test__line_to_row(line: str, expected_result: navigation_table.TableRow):
+    """
+    arrange: given line and expected return value
+    act: when _line_to_row is called with the line
+    assert: then the expected return value is returned.
+    """
+    returned_result = navigation_table._line_to_row(line)
+
+    assert returned_result == expected_result
+
+
+def test__line_to_row_no_match():
+    """
+    arrange: given line that does not match
+    act: when _line_to_row is called with the line
+    assert: then NavigationTableParseError is raised.
+    """
+    with pytest.raises(NavigationTableParseError):
+        navigation_table._line_to_row("")
+
+
+@pytest.mark.parametrize(
+    "page, expected_table",
+    [
+        pytest.param("", [], id="empty"),
+        pytest.param("|level|path|navlink|", [], id="header only"),
+        pytest.param("|level|path|navlink|\n|-|-|-|", [], id="header, filler"),
+        pytest.param(
+            "|level|path|navlink|\n|-|-|-|\n|1|a|[b]()|",
+            [(1, "a", ("b", None))],
+            id="header, filler and single row",
+        ),
+        pytest.param(
+            "|level|path|navlink|\n|1|a|[b]()|",
+            [(1, "a", ("b", None))],
+            id="header, single row",
+        ),
+        pytest.param(
+            "text 1\n|level|path|navlink|\n|1|a|[b]()|",
+            [(1, "a", ("b", None))],
+            id="text, header, single row",
+        ),
+        pytest.param(
+            "|level|path|navlink|\n|1|a|[b]()|\n|2|c|[d]()|",
+            [(1, "a", ("b", None)), (2, "c", ("d", None))],
+            id="header, multiple rows",
+        ),
+    ],
+)
+def test_from_page(page: str, expected_table: list[navigation_table.TableRow]):
+    """
+    arrange: given page and expected table
+    act: when from_page is called with the page
+    assert: then the expected rtable is returned.
+    """
+    returned_table = navigation_table.from_page(page=page)
+
+    assert returned_table == expected_table
+
+
+def test_from_page_indico():
+    """
+    arrange: given Indico's navigation page
+    act: when from_page is called with the page
+    assert: then the page is parsed.
+    """
+    indico_page = """Indico is an open-source tool for event organisation, archival and collaboration, catering to lectures, meetings, workshops and conferences.
+
+For details on Indico's features, see [this page](https://getindico.io/features/).
+
+# Navigation
+
+| Level | Path | Navlink |
+| -- | -- | -- |
+| 1 | tutorials | [Tutorials]() |
+| 1 | how-to-guides | [How-to guides]() |
+| 2 | contributing | [Contributing](/t/indico-docs-contributing/6574)|
+| 2 | cross-model-db-relations | [Cross-model DB relations](/t/indico-docs-cross-model-relations-for-pg/7009)|
+| 2 | refresh-external-resources | [Refreshing external resources](/t/indico-docs-refreshing-external-resources/7008) |
+| 1 | reference | [Reference]() |
+| 2 | plugins | [Plugins](/t/indico-docs-plugins/6553) |
+| 2 | theme-customisation | [Theme Customisation](/t/indico-docs-themes/6554) |
+| 1 | explanation | [Explanation]() |
+| 2 | charm-architecture | [Charm Architecture](/t/indico-docs-charm-architecture/7010) |"""
+
+    returned_table = navigation_table.from_page(page=indico_page)
+
+    assert returned_table == [
+        (1, "tutorials", ("Tutorials", None)),
+        (1, "how-to-guides", ("How-to guides", None)),
+        (2, "contributing", ("Contributing", "/t/indico-docs-contributing/6574")),
+        (
+            2,
+            "cross-model-db-relations",
+            ("Cross-model DB relations", "/t/indico-docs-cross-model-relations-for-pg/7009"),
+        ),
+        (
+            2,
+            "refresh-external-resources",
+            ("Refreshing external resources", "/t/indico-docs-refreshing-external-resources/7008"),
+        ),
+        (1, "reference", ("Reference", None)),
+        (2, "plugins", ("Plugins", "/t/indico-docs-plugins/6553")),
+        (2, "theme-customisation", ("Theme Customisation", "/t/indico-docs-themes/6554")),
+        (1, "explanation", ("Explanation", None)),
+        (
+            2,
+            "charm-architecture",
+            ("Charm Architecture", "/t/indico-docs-charm-architecture/7010"),
+        ),
+    ]
