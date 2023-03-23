63faa567ee8a96f2424cdf3aeae2f2024804eadb
jdkandersson <david.andersson@canonical.com>
2023-03-23 15:36:24 +1100

    refactor

--- src/check.py
+++ src/check.py
@@ -27,23 +27,34 @@ def _is_update_action(action: AnyAction) -> TypeGuard[UpdateAction]:
 
     Args:
         action: The action to check.
+
+    Returns:
+        Whether the action is an UpdateAction.
     """
     return isinstance(action, UpdateAction)
 
 
-def _action_conflicts(action: UpdateAction) -> str | None:
-    """Get any conflicts the action has.
+def _update_action_problem(action: UpdateAction) -> Problem | None:
+    """Get any problem with an update action.
 
     Args:
         action: The action to check.
+
+    Returns:
+        None if there is no problem or the problem if there is an issue with the action.
     """
     if action.content_change is None:
         return None
-    return content_conflicts(
+
+    action_conflcits = content_conflicts(
         base=action.content_change.base,
         theirs=action.content_change.old,
         ours=action.content_change.new,
     )
+    if action_conflcits is None:
+        return None
+
+    return Problem(path=action.path, description=action_conflcits)
 
 
 def conflicts(actions: Iterable[AnyAction]) -> Iterator[Problem]:
@@ -56,8 +67,4 @@ def conflicts(actions: Iterable[AnyAction]) -> Iterator[Problem]:
         A problem for each action with a conflict
     """
     update_actions = filter(_is_update_action, actions)
-    yield from (
-        Problem(path=action.path, description=conflicts)
-        for action in update_actions
-        if (conflicts := _action_conflicts(action))
-    )
+    yield from filter(None, (_update_action_problem(action) for action in update_actions))
