456dc628f2914f4dfb225cce7ead337b4f880acd
jdkandersson <david.andersson@canonical.com>
2023-08-04 13:40:41 +1000

    add additional tests for calculating the hierarchy

--- tests/unit/test_index_contents_hierarchy.py
+++ tests/unit/test_index_contents_hierarchy.py
@@ -564,6 +564,32 @@ def _test__calculate_contents_hierarchy_parameters():
                     whitespace_count=0, reference_value=(value_1 := "dir_1")
                 ),
                 item_2 := factories.IndexParsedListItemFactory(
+                    whitespace_count=0, reference_value=(value_2 := "https://canonical.com")
+                ),
+            ),
+            ("dir", "skip"),
+            (
+                factories.IndexContentsListItemFactory(
+                    hierarchy=1,
+                    reference_title=item_1.reference_title,
+                    reference_value=value_1,
+                    rank=item_1.rank,
+                ),
+                factories.IndexContentsListItemFactory(
+                    hierarchy=1,
+                    reference_title=item_2.reference_title,
+                    reference_value=value_2,
+                    rank=item_2.rank,
+                ),
+            ),
+            id="single directory single external link",
+        ),
+        pytest.param(
+            (
+                item_1 := factories.IndexParsedListItemFactory(
+                    whitespace_count=0, reference_value=(value_1 := "dir_1")
+                ),
+                item_2 := factories.IndexParsedListItemFactory(
                     whitespace_count=1, reference_value=(value_2 := f"{value_1}/file_2.md")
                 ),
             ),
