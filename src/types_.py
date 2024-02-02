1f56e7392088a4217043283de916361e04ed4e46
mthaddon <tom.haddon@canonical.com>
2024-02-02 12:55:12 +0100

    Add a formatted representation of the UpdatePageAction dataclass (#230)
    * Add a formatted representation of the UpdatePageAction dataclass
    
    * Update changelog
    
    * Add return value to docstring
    
    * Include return type
    
    * Apply lint changes
    
    * Include a string representation of the class, which many tests check for

--- src/types_.py
+++ src/types_.py
@@ -389,6 +389,18 @@ class UpdatePageAction(_UpdateActionBase):
 
     content_change: ContentChange
 
+    def __str__(self) -> str:
+        """Return a formatted representation of the dataclass.
+
+        Returns:
+            Formatted representation of the dataclass.
+        """
+        return (
+            f"class: {self.__class__}, level: {self.level}, path: {self.path}, "
+            f"navlink_change: {self.navlink_change}\n"
+            f"content_change:\n{self.content_change}"
+        )
+
 
 @dataclasses.dataclass
 class UpdateExternalRefAction(_UpdateActionBase):
