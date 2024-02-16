49c561ea48d7fd507bc31984543b5768120b9f56
Yanks Yoon <yanksyoon@canonical.com>
2024-02-16 16:01:18 +0000

    Removed `discourse-gatekeeper/discourse-ahead-ok` (#235)
    * feat(ISD-1567): removed obsolete code and corresponding documentation
    
    * feat(ISD-1498): removed check in test_check for the removed tag
    
    * feat(ISD-1498): updated test parameters to account for removal of discourse ahead tag removal
    
    * feat(ISD-1498): removed tagged argument from tests
    
    * feat(ISD-1498): ensured all parameters return only 2 values in tuple
    
    * feat(ISD-1498): removed ahead tag from constants file, removed mention in integration tests
    
    * feat(ISD-1498): removed ahead tag from check.py
    
    * feat(ISD-1498): rectified erroneous deletion of expected problem in test check
    
    * feat(ISD-1498): adhered to linting requirements by removing unused src.constants imports
    
    * feat(ISD-1498): fixed all linting issues and fixed method signatures.
    
    * feat(ISD-1498): removed test case 7 from test conflicts
    
    * feat(ISD-1498): removed test 7
    
    * feat(ISD-1498): fixed linting issues
    
    * feat(ISD-1498): updated change log with new version details
    
    * feat(ISD-1498): added src-docs
    
    * feat(ISD-1498): fixed comments in run_conflict integration test
    
    * feat(ISD-1498): removed assert associated with removed act

--- src/check.py
+++ src/check.py
@@ -10,9 +10,8 @@ from typing import NamedTuple, TypeGuard
 
 import requests
 
-from src import constants, content
+from src import content
 from src.constants import DOCUMENTATION_TAG
-from src.repository import Client
 from src.types_ import (
     AnyAction,
     IndexContentsListItem,
@@ -20,7 +19,6 @@ from src.types_ import (
     UpdateExternalRefAction,
     UpdateGroupAction,
     UpdatePageAction,
-    UserInputs,
 )
 
 
@@ -158,9 +156,7 @@ def _update_action_problem(action: UpdateAction) -> Problem | None:
     return problem
 
 
-def conflicts(
-    actions: Iterable[AnyAction], repository: Client, user_inputs: UserInputs
-) -> Iterator[Problem]:
+def conflicts(actions: Iterable[AnyAction]) -> Iterator[Problem]:
     """Check whether actions have any content conflicts.
 
     There are two types of conflicts. The first is where the local content is different to what is
@@ -176,8 +172,6 @@ def conflicts(
 
     Args:
         actions: The actions to check.
-        repository: Client for repository interactions.
-        user_inputs: Configuration from the user.
 
     Yields:
         A problem for each action with a conflict
@@ -196,12 +190,6 @@ def conflicts(
     if any_page_conflicts:
         return
 
-    commit_discourse_ahead_ok_tagged = repository.is_same_commit(
-        tag=constants.DISCOURSE_AHEAD_TAG, commit=user_inputs.commit_sha
-    )
-    if commit_discourse_ahead_ok_tagged:
-        return
-
     paths_with_diff = get_path_with_diffs(actions_logical_conflicts)
     if not paths_with_diff.base_local_diffs or not paths_with_diff.base_server_diffs:
         return
@@ -210,9 +198,7 @@ def conflicts(
         path=paths_with_diff.base_local_diffs[0],
         description=(
             "detected unmerged community contributions, these need to be resolved "
-            "before proceeding. If the differences are not conflicting, please apply the "
-            f"{constants.DISCOURSE_AHEAD_TAG} tag to commit {user_inputs.commit_sha} to "
-            "proceed. Paths with potentially unmerged community contributions: "
+            "before proceeding.  Paths with potentially unmerged community contributions: "
             f"{set(chain(paths_with_diff.base_local_diffs, paths_with_diff.base_server_diffs))}."
         ),
     )
